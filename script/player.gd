d729de9653cac7c65d0f6bac7d8fe8b2d929f994
lucasjanta <lucastyle2014@gmail.com>
2024-03-05 16:49:22 -0300

    Simple ranged combat and collision with world elements

--- script/player.gd
+++ script/player.gd
@@ -11,7 +11,12 @@ var bow_equipped = false
 var bow_cooldown = true
 var arrow = preload("res://scenes/arrow.tscn")
 
+var mouse_loc_from_player = null
+
 func _physics_process(delta):
+	mouse_loc_from_player = get_global_mouse_position() - self.position
+	#print(mouse_loc_from_player)
+	
 	var direction = Input.get_vector("left", "right", "up", "down")
 	
 	if direction.x == 0 and direction.y == 0:
@@ -42,30 +47,49 @@ func _physics_process(delta):
 	play_anim(direction)
 	
 func play_anim(dir):
-	if player_state == "idle":
-		anim.play("idle")
-	if player_state == "walking":
-		if dir.y == -1:
-			anim.play("n-walk")
-		if dir.x == 1:
-			anim.play("e-walk")
-		if dir.y == 1:
-			anim.play("s-walk")
-		if dir.x == -1:
-			anim.play("w-walk")
+	if !bow_equipped:
+		speed = 100
+		if player_state == "idle":
+			anim.play("idle")
+		if player_state == "walking":
+			if dir.y == -1:
+				anim.play("n-walk")
+			if dir.x == 1:
+				anim.play("e-walk")
+			if dir.y == 1:
+				anim.play("s-walk")
+			if dir.x == -1:
+				anim.play("w-walk")
+				
+			if dir.x > 0.5 and dir.y < -0.5:
+				anim.play("ne-walk")
+			if dir.x > 0.5 and dir.y > 0.5:
+				anim.play("se-walk")
+			if dir.x < -0.5 and dir.y < -0.5:
+				anim.play("nw-walk")
+			if dir.x < -0.5 and dir.y > 0.5:
+				anim.play("sw-walk")
+	if bow_equipped:
+		speed = 50
+		if mouse_loc_from_player.x >= -25 and mouse_loc_from_player.x <= 25 and mouse_loc_from_player.y < 0:
+			anim.play("n-attack")
+		if mouse_loc_from_player.y >= -25 and mouse_loc_from_player.y <= 25 and mouse_loc_from_player.x >  0:
+			anim.play("e-attack")
+		if mouse_loc_from_player.x >= -25 and mouse_loc_from_player.x <= 25 and mouse_loc_from_player.y > 0:
+			anim.play("s-attack")
+		if mouse_loc_from_player.y >= -25 and mouse_loc_from_player.y <= 25 and mouse_loc_from_player.x < 0:
+			anim.play("w-attack")
 			
-		if dir.x > 0.5 and dir.y < -0.5:
-			anim.play("ne-walk")
-		if dir.x > 0.5 and dir.y > 0.5:
-			anim.play("se-walk")
-		if dir.x < -0.5 and dir.y < -0.5:
-			anim.play("nw-walk")
-		if dir.x < -0.5 and dir.y > 0.5:
-			anim.play("sw-walk")
-			
+		if mouse_loc_from_player.x >= 25 and mouse_loc_from_player.y <= -25:
+			anim.play("ne-attack")
+		if mouse_loc_from_player.x >= 0.5 and mouse_loc_from_player.y >= 25:
+			anim.play("se-attack")
+		if mouse_loc_from_player.x <= -0.5 and mouse_loc_from_player.y >= -5:
+			anim.play("sw-attack")
+		if mouse_loc_from_player.x <= -25 and mouse_loc_from_player.y <= -25:
+			anim.play("nw-attack")
 func player():
 	pass
-		
 func collect(item):
 	inv.insert(item)
 
